f6d00ffb78f33e88bd19d7e116bc896477a3a432
yeoseyoung0502 <dutpdud132@sookmyung.ac.kr>
2023-06-20 18:07:04 +0900

    백엔드 개발 10 @PathVariable 사용하기 추가

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("test")
@@ -10,5 +12,9 @@ public class TestController {
 	@GetMapping
 	public String testController(){
 		return "Hello World!(안녕하세요!)";
+	}
+	@GetMapping("/{id}")
+	public String testContollerWithPathVariables(@PathVariable(required =false) int id){
+		return "Hello World! ID "+ id; 
 	}
 }
